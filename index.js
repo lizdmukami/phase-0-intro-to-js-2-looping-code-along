d399651c21ddfbe1c453b107aa12a3ed2d85dbca
Moringa School <lizdmukami@gmail.com>
2022-11-19 14:09:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+const gifts = [];
+
+function writeCards(gifts, surprise) {
+    for (let i = 0; i < gifts.length; i++) {
+    gifts[i] = `Thank you, ${gifts[i]}, for the wonderful ${surprise} gift!`;
+    }
+    
+    return gifts;
+}
+
+    console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+    function countDown(number) {
+    
+        while (number >= 0) {
+            console.log(number--);
+            debugger;
+        }
+    }
+        countDown(4)
